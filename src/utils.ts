031c2a7e994129ad3d9ee91a6dc99934cf27e3c1
Clayton Matule <clayton.matule@hotmail.com>
2024-01-23 00:42:44 +0200

    wip

--- src/utils.ts
+++ src/utils.ts
@@ -33,7 +33,7 @@ export async function sanitizeVideo(fileName: string): Promise<string> {
         videoName
     );
     return new Promise((resolve, reject) => {
-        ffmpeg().input(fileName).outputOptions('-c:v', 'libx264', '-crf', '23', `${videoName}`).saveToFile(videoName).on('end', () => {
+        ffmpeg().input(fileName).outputOptions('-c:v', 'libx264', '-crf', '23', `-preset`, 'fast', '-c:a', 'aac', '-b:a', '192k', '-ac', '2').saveToFile(videoName).on('end', () => {
             return resolve(videoName)
         }).on('error', (error) => {
             return reject(error);
